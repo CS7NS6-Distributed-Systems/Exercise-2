dc4a7f3363d5692b097f8464ceac853b0da7ac86
Harinath Babu <harinath.b.07@gmail.com>
2025-03-15 14:32:22 +0000

    session cached to redis and logout route added

--- booking-platform/booking-service/app/user_routes.py
+++ booking-platform/booking-service/app/user_routes.py
@@ -1,14 +1,26 @@
 from flask import Blueprint, request, jsonify
 from flask_bcrypt import Bcrypt
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask import send_file
 import psycopg2
 from pymongo.errors import PyMongoError
 from datetime import timedelta
+import io
 
-from app.db import cockroach_conn, mongo_db
+from app.db import cockroach_conn, mongo_db, redis_client
 
 user_blueprint = Blueprint('user', __name__)
 bcrypt = Bcrypt()
+
+def session_required(fn):
+    @wraps(fn)
+    @jwt_required()
+    def decorated_fn(*args, **kwargs):
+        username = get_jwt_identity()
+        if not redis_client.get(f"session: {username}"):
+            return jsonify({"error": "Session expired. Log in again"}), 401
+        return fn(*args, **kwargs)
+    return decorated_fn
 
 @user_blueprint.route('/register', methods=['POST'])
 def register():
@@ -75,6 +87,7 @@ def login():
         password_hash = user_record[0]
         if bcrypt.check_password_hash(password_hash, password):
             token = create_access_token(identity=username, expires_delta=timedelta(hours=1))
+            redis_client.setex({f"session: {username}"}, timedelta(hours=1), token)
             return jsonify({"message": "Login successful", "access_token": token}), 200
 
         return jsonify({"error": "Invalid username or password"}), 401
@@ -83,3 +96,13 @@ def login():
         return jsonify({"error": "Database error", "details": str(e)}), 500
     except Exception as e:
         return jsonify({"error": "An unexpected error occurred", "details": str(e)}), 500
+    
+@user_blueprint.route('/logout', methods=['POST'])
+def logout():
+    try:
+        username = get_jwt_identity()
+        redis_client.delete(f"session: {username}")
+        return jsonify({"message": "logout successful"})
+    except Exception as e:
+        return jsonify({"error":"An error occured", "details": str(e)}), 500
+
