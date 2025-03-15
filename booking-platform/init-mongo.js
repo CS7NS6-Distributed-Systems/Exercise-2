2a2adaa78a5ad92744d6f9ee728a8bf8b0826a38
Harinath Babu <harinath.b.07@gmail.com>
2025-03-15 00:52:37 +0000

    collection env changed

--- booking-platform/init-mongo.js
+++ booking-platform/init-mongo.js
@@ -2,7 +2,7 @@
 db = db.getSiblingDB("booking_db");
 
 // Create users collection (if it doesn't exist)
-db.createCollection("users");
+db.createCollection("user_licenses");
 
 // Ensure username is unique
 db.users.createIndex({ "username": 1 }, { unique: true });
