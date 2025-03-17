e2c90b3b93790ee1d710d57448f5f633f8247b6b
Harinath Babu <harinath.b.07@gmail.com>
2025-03-17 19:31:59 +0000

    limited added and app logic moved to __init__

--- booking-platform/booking-service/app/app.py
+++ booking-platform/booking-service/app/app.py
@@ -1,31 +1,4 @@
-from flask import Flask
-from flask_jwt_extended import JWTManager
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-
-from app.user_routes import user_blueprint
-from app.const import RATE_LIMIT_GLOBAL
-
-app = Flask(__name__)
-
-# JWT Configuration
-app.config["JWT_SECRET_KEY"] = "tempsecretkey"
-jwt = JWTManager(app)
-
-# Rate Limiter Configuration
-limiter = Limiter(
-    get_remote_address,
-    app=app,
-    default_limits=RATE_LIMIT_GLOBAL,
-)
-
-# Default route
-@app.route('/')
-def hello():
-    return "Welcome to the Booking Platform!"
-
-# Register Blueprints
-app.register_blueprint(user_blueprint, url_prefix='/user')
+from app import app
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
