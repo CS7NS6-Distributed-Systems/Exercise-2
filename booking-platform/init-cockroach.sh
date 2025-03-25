b88512e37e19d104e6d0de99699763cacec90ebe
AayushGala-git <aayushgala19@gmail.com>
2025-03-25 14:08:32 +0000

    Focus only on nginx load balancer, remove CockroachDB customizations

--- booking-platform/init-cockroach.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Create the backup directory if it doesn't exist
-mkdir -p /cockroach/cockroach-data/extern/1/backup
-
-# Start CockroachDB
-exec /cockroach/cockroach start-single-node --insecure "$@" 
